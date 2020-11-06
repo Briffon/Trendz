efcfb7e8e20839dd467eb2e95c13fd6f9322fb8f
rushnightfir3 <ashyoda@gmail.com>
2020-11-05 22:24:26 -0500

    sass for header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./components/routes";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
-import "./App.css";
+import "./main.scss";
 
 function App() {
   return (
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,14 +4,25 @@ const Header = () => {
   return (
     <div class="header">
       <img
+        class="logo"
         src="https://github.com/Briffon/Trendz/blob/main/src/assets/logo.png?raw=true"
         alt="Trendz logo"
       />
-      <input type="text" class="searchbar" />
-      <img
-        src="https://github.com/Briffon/Trendz/blob/main/src/assets/cart.png?raw=true"
-        alt="shopping cart"
-      />
+      <div class="interaction">
+        <form class="search">
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/622/622669.svg"
+            alt="search icon"
+            class="search-icon"
+          />
+          <input type="text" class="searchbar" />
+        </form>
+        <img
+          class="shopping-cart"
+          src="https://github.com/Briffon/Trendz/blob/main/src/assets/cart.png?raw=true"
+          alt="shopping cart"
+        />
+      </div>
     </div>
   );
 };
